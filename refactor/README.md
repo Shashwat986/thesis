838d55b576e7467ce64b22d48c8b6dbfe8f4214e
Shashwat Chandra <chandra.shashwat@gmail.com>
2017-09-06 13:12:46 +0530

    Adding vector generation code to run.py

--- refactor/README.md
+++ refactor/README.md
@@ -1,1 +1,3 @@
 Step 1: Adding GloVe and Word2Vec implementations to the repo.
+Step 2: Creating `glove.sh` and `word2vec.sh` to generate word vectors
+Step 3: Add code to `run.py` to generate vectors from the class instance
--- refactor/run.py
+++ refactor/run.py
@@ -1,12 +1,12 @@
 #!python
 import subprocess, os
-current_env = os.environ.copy()
 
 # Step 1
 # Creation of word vectors from corpus
 
 class VectorAlignment:
-  def __init__(self, file_name = None, options = None):
+  def __init__(self, file_name = None, output_file_name = None, options = None):
+    self.env = os.environ.copy()
     if file_name is None:
       print("Downloading test corpus")
       subprocess.call(['curl', '-O', 'http://mattmahoney.net/dc/text8.zip'])
@@ -16,28 +16,46 @@ class VectorAlignment:
     else:
       self.file_name = file_name
 
+    if output_file_name is None:
+      self.output_file_name = 'vectors'
+    else:
+      self.output_file_name = output_file_name
+
     if options is None:
       self.options = {}
     else:
       self.options = options
 
+  def set_file_name(file_name):
+    self.file_name = file_name
+
+  def set_output_file_name(file_name):
+    self.output_file_name = file_name
+
   def set_default_values(self):
-    if self.options.get('d') is None:
-      self.options['d'] = 200
-
-
+    for key in ['VECTOR_SIZE', 'WINDOW_SIZE', 'VOCAB_MIN_COUNT']:
+      if key in self.options:
+        self.env[key] = str(self.options[key])
 
   def generate_vectors(self, method="glove"):
+    if method not in ['glove', 'word2vec']:
+      return None
+
     print("Using {} to generate word vectors.".format(method))
 
-    # Setting default values for required params
+    self.set_default_values()
 
+    exit_code = subprocess.call([
+      method + '.sh',
+      self.file_name,
+      self.output_file_name
+    ], env=self.env)
 
+    if exit_code == 0:
+      print("Vectors created: {}".format(self.output_file_name))
+    else:
+      raise Exception("Error in word vector generation")
 
-
-
-
-
-
-
+  def align_vectors(self, other):
+    pass
 
